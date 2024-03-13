e113e93e8a99e1b0e9b46833469e7eb193f679f8
Matheus <matheusfariasdasneves@gmail.com>
2024-03-13 13:50:46 -0300

    feat: add InputComponent

---
+++ src/components/input-component/index.jsx
@@ -0,0 +1,15 @@
+const InputComponent = ({ label, type, placeholder, onChange }) => {
+  return (
+    <>
+      <span className="text-whiteColor font-medium">{`${label}:`}</span>
+      <input
+        type={type}
+        placeholder={placeholder}
+        className="w-full p-2 border border-gray-300 rounded-md"
+        onChange={onChange}
+      />
+    </>
+  );
+};
+
+export default InputComponent;
