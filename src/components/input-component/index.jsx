b66afd210dc6eebc0e75a059562f92d4f7993ee1
Matheus <matheusfariasdasneves@gmail.com>
2024-03-13 20:30:04 -0300

    feat: add value property to InputComponent

--- src/components/input-component/index.jsx
+++ src/components/input-component/index.jsx
@@ -1,11 +1,12 @@
-const InputComponent = ({ label, type, placeholder, onChange }) => {
+const InputComponent = ({ label, type, placeholder, value, onChange }) => {
   return (
     <>
       <span className="text-whiteColor font-medium">{`${label}:`}</span>
       <input
+        className="w-full p-2 border border-gray-300 rounded-md"
         type={type}
         placeholder={placeholder}
-        className="w-full p-2 border border-gray-300 rounded-md"
+        value={value}
         onChange={onChange}
         required
       />
