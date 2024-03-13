9c600f349fd68ab86fc6bf3e46327c9283b3e756
Matheus <matheusfariasdasneves@gmail.com>
2024-03-13 18:09:20 -0300

    refactor: ensure spacing is handled by the Button Submit parent component

--- src/components/button-submit/index.jsx
+++ src/components/button-submit/index.jsx
@@ -2,7 +2,7 @@ const ButtonSubmit = ({ children }) => {
   return (
     <button
       type="submit"
-      className="bg-greenColor ml-auto text-lg text-whiteColor font-bold px-8 py-2 rounded-xl mt-12 hover:scale-105 transition-all duration-300 ease-in-out"
+      className="bg-greenColor text-lg text-whiteColor font-bold px-8 py-2 rounded-xl hover:scale-105 transition-all duration-300 ease-in-out"
     >
       {children}
     </button>
